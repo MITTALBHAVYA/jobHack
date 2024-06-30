d657243f977a9c7840bd3bddbb4f729709f58e7b
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-06-30 18:11:43 +0530

    JobDetails done

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -1,9 +1,61 @@
+import {useContext,useEffect,useState} from 'react';
+import axios from 'axios';
+import {Link,useParams,useNavigate} from 'react-router-dom';
+import {Context} from '../../main';
+
 const JobDetails = () => {
+  const {jobId}=useParams();
+  const [job,setJob]=useState({});
+  const [loading,setLoading]=useState(true);
+  const [error,setError]=useState(null);
+  const navigateTo=useNavigate();
+  const {isAuthorized,user}=useContext(Context);
+
+  useEffect(()=>{
+    if(!isAuthorized){
+      navigateTo("/login");
+    }
+    const fetchJobDetails = async ()=>{
+      try{
+        const {data}=await axios.get(`http://localhost:3000/api/v1/job/${jobId}`,{withCredentials:true});
+        setJob(data.job);
+      }
+      catch(err){
+        setError(err.response?.data?.message || err.message);
+        navigateTo("/notfound");
+      }
+      finally{
+        setLoading(false);
+      }
+    };
+    fetchJobDetails();
+  },[jobId,isAuthorized,navigateTo]);
+
+  if(loading){
+    return <div>Loading...</div>;
+  }
+
+  if(error){
+    return <div>{error}</div>;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <section className="jobDetails page">
+      <div className="container">
+        <h3>Job Details</h3>
+        <div className="banner">
+          <p>Title: <span>{job.title}</span></p>
+          <p>Category: <span>{job.category}</span> </p>
+          <p>Country: <span>{job.country}</span></p>
+          <p>Location: <span>{job.location}</span></p>
+          <p>Description: <span>{job.description}</span></p>
+          <p>Job Posted On: <span>{job.jobPostedOn}</span></p>
+          <p>Salary: {job.fixedSalary?<span>{job.fixedSalary}</span> : <span>{job.salaryFrom} - {job.salaryTo}</span>}</p>
+          {user && user.role !== "Employer" && (<Link to={`/application/${job._id}`}>Apply Now</Link>)}
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default JobDetails
+export default JobDetails;
