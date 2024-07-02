de774d248f74c68126d18c55c9b49a63a001966c
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-07-02 19:40:39 +0530

    netlify deploy

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 dotenv.config({ path: './config/config.env' });
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: ["https://jobhack1729.netlify.app"],
     methods: ['GET', 'POST', 'DELETE', 'PUT'],
     credentials: true,
 })
