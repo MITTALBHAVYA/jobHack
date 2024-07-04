ea1192af9968b9d0ad9669d406ca7a1f99a4c6e9
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2024-07-04 21:21:41 +0530

    consoleparts

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/userSchema.js";
 export const isAuthorized = catchAsyncError(async(req,res,next)=>{
     console.log("check auth...")
+    console.log(req.cookies.token," ",req.headers.authorization)
     const token =
     req.cookies.token ||
     (req.headers.authorization && req.headers.authorization.split(" ")[1]);
